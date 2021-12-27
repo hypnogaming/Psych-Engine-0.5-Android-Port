561e4fa42878ee540913f8243b42e02ca25ee47a
hypnogaming <96710025+hypnogaming@users.noreply.github.com>
2021-12-27 00:39:41 -0300

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -20,6 +20,7 @@ class CoolUtil
 		'Easy',
 		'Normal',
 		'Hard'
+                'Legacy'
 	];
 	public static var defaultDifficulty:String = 'Normal'; //The chart that has no suffix and starting difficulty on Freeplay/Story Mode
 
